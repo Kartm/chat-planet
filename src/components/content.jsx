6aea8813624f1445047e20a8765e04b8926c43a4
Łukasz Blachnicki <superkwek0@gmail.com>
2018-12-27 12:04:03 +0100

    UI improvements.

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,26 +1,26 @@
-import React, { Component } from "react";
-import Map from "./map";
-import About from "./about";
-import Chat from "./chat";
+import React, { Component } from 'react';
+import Map from './map';
+import About from './about';
+import Chat from './chat';
 
 class Content extends Component {
-	getPanel = () => {
-		switch (this.props.panel) {
-			case 0:
-				return <Map />;
-			case 1:
-				return <About />;
-			case 2:
-				return <Chat />;
-		}
-	};
-	render() {
-		return (
-			<React.Fragment>
-				<div className="content-wrapper">{this.getPanel()}</div>
-			</React.Fragment>
-		);
-	}
+  getPanel = () => {
+    switch (this.props.panel) {
+      case 1:
+        return <About />;
+      case 2:
+        return <Chat />;
+      default:
+        return <Map />;
+    }
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <div className='content-wrapper'>{this.getPanel()}</div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Content;
--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,106 +1,84 @@
-import React, { Component } from "react";
-import logo from "../resources/graphics/map2.svg";
-import MarkerIcon from "../resources/graphics/marker.png";
+import React, { Component } from 'react';
+import logo from '../resources/graphics/map2.svg';
+import MarkerIcon from '../resources/graphics/marker.png';
 
 class Marker extends Component {
-	//todo set size according to count!
-	componentWillMount() {
-		this.setState(
-			{
-				count: this.props.count
-			},
-			this.getPosition
-		);
-	}
+  //todo set size according to count!
+  componentWillMount() {
+    this.setState(
+      {
+        count: this.props.count
+      },
+      this.getPosition
+    );
+  }
 
-	getPosition = () => {
-		let windowWidth = document.documentElement.clientWidth;
-		let windowHeight = document.documentElement.clientHeight;
+  getPosition = () => {
+    let windowWidth = document.documentElement.clientWidth;
+    let windowHeight = document.documentElement.clientHeight;
 
-		let y = (this.props.y / windowHeight) * 100;
-		y = +y.toFixed(2);
-		let x = (this.props.x / windowWidth) * 100;
-		x = +x.toFixed(2);
+    let y = (this.props.y / windowHeight) * 100;
+    y = +y.toFixed(2);
+    let x = (this.props.x / windowWidth) * 100;
+    x = +x.toFixed(2);
 
-		console.log(x, y);
-		this.setState({
-			top: `${y}vh`,
-			left: `${x}vw`
-		});
-	};
+    console.log(x, y);
+    this.setState({
+      top: `${y}vh`,
+      left: `${x}vw`
+    });
+  };
 
-	render() {
-		return (
-			<React.Fragment>
-				<div
-					style={{ top: this.state.top, left: this.state.left }}
-					className="marker-wrapper"
-				>
-					<div className="marker">
-						<img className="marker-image" src={MarkerIcon} />
-						<div className="marker-number small-shadow">
-							{this.props.count}
-						</div>
-					</div>
-				</div>
-			</React.Fragment>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <div style={{ top: this.state.top, left: this.state.left }} className='marker-wrapper'>
+          <div className='marker'>
+            <img className='marker-image' src={MarkerIcon} alt='Country pin.' />
+            <div className='marker-number small-shadow'>{this.props.count}</div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 class Map extends Component {
-	state = {
-		index: 1,
-		positions: [
-			/*{ x: 0, y: 0, key: 0, count: 12 }*/
-		],
-		biggestCount: 12
-	};
+  state = {
+    index: 1,
+    positions: [
+      /*{ x: 0, y: 0, key: 0, count: 12 }*/
+    ],
+    biggestCount: 12
+  };
 
-	generateMarkers = () => {
-		if (this.state.positions.length > 0)
-			return this.state.positions.map(el => (
-				<Marker
-					key={el.key}
-					x={el.x}
-					y={el.y}
-					count={el.count}
-					biggestCount={this.state.biggestCount}
-				/>
-			));
-	};
+  generateMarkers = () => {
+    if (this.state.positions.length > 0)
+      return this.state.positions.map(el => (
+        <Marker key={el.key} x={el.x} y={el.y} count={el.count} biggestCount={this.state.biggestCount} />
+      ));
+  };
 
-	testAddMarker = e => {
-		let x = Math.round(
-			e.clientX - e.target.getBoundingClientRect().left + window.scrollX
-		);
-		let y = Math.round(
-			e.clientY - e.target.getBoundingClientRect().top + window.scrollY
-		);
-		let marker = { key: this.state.index, x: x, y: y, count: 12 };
-		this.setState({ positions: [...this.state.positions, marker] });
-		this.setState({ index: this.state.index + 1 });
-	};
+  testAddMarker = e => {
+    let x = Math.round(e.clientX - e.target.getBoundingClientRect().left + window.scrollX);
+    let y = Math.round(e.clientY - e.target.getBoundingClientRect().top + window.scrollY);
+    let marker = { key: this.state.index, x: x, y: y, count: 12 };
+    this.setState({ positions: [...this.state.positions, marker] });
+    this.setState({ index: this.state.index + 1 });
+  };
 
-	render() {
-		return (
-			<React.Fragment>
-				<div className="map-wrapper">
-					<img
-						src={logo}
-						className="map-img medium-shadow"
-						alt="World map"
-					/>
-					<div
-						onClick={e => this.testAddMarker(e)}
-						className="map-img-overlay"
-					>
-						{this.generateMarkers()}
-					</div>
-				</div>
-			</React.Fragment>
-		);
-	}
+  render() {
+    return (
+      <React.Fragment>
+        <div className='map-wrapper'>
+          <img src={logo} className='map-img medium-shadow' alt='World map' />
+          <div onClick={e => this.testAddMarker(e)} className='map-img-overlay'>
+            {this.generateMarkers()}
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Map;
