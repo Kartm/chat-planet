bdf096ae50d54782ae460bc961478082b97965d7
Kartm <lblachnicki@gmail.com>
2019-06-30 12:17:13 +0200

    Add automatic chat scrolling

--- src/components/Chat/ChatMessages/ChatMessages.js
+++ src/components/Chat/ChatMessages/ChatMessages.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import './ChatMessages.css'
 import ChatMessage from './ChatMessage/ChatMessage'
 
 const ChatMessages = props => {
+    const scrollView = useRef(null)
+
     let result = []
 
     let { messages, user } = props
@@ -16,10 +18,21 @@ const ChatMessages = props => {
         )
     })
 
+    useEffect(() => {
+        console.log(scrollView.current.clientHeight)
+        console.log(scrollView.current.scrollHeight)
+        scrollView.current.scrollTo(0, scrollView.current.scrollHeight)
+    })
+
     return (
         <div className='chat-text-wrapper'>
             <div className='chat-messages-wrapper'>
-                <div className='chat-messages-wrapper-scrollview'>{result}</div>
+                <div
+                    ref={scrollView}
+                    className='chat-messages-wrapper-scrollview'
+                >
+                    {result}
+                </div>
             </div>
         </div>
     )
